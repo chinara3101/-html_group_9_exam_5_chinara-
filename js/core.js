5327e3b77b651f4bf22555b79ae5fc53a8d6e084
Chinara1214 <you@example.com>
2017-05-13 19:29:49 +0600

    заканчиваю работу

--- js/core.js
+++ js/core.js
@@ -1,14 +1,11 @@
-$(document).ready(function(){
 
-	$('#menu-btn').click(function(){		
-			$('.menu').slideToggle(700);	
-			if ($(this).hasClass('not-active')) {
-				$(this).addClass('is-active').removeClass('not-active');
-			}else{
-				setTimeout(function(){
-					$('.is-active').addClass('not-active').removeClass('is-active')
-				},600)			
-			}		
-		});
 
+	$(document).ready(function(){
+
+	$('.slider-1').slick({
+    arrows: true
+	});
 });
+
+
+	
